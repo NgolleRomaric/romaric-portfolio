635e34ed673d4c97efac7c1b7535b7fdef864c77
NgollrRomaric <ngolleromaric27@gmail.com>
2025-05-08 23:36:17 +0100

    feat(Projects): add project cards with detailed descriptions and badges
    This commit introduces project cards in the Projects section, showcasing real-world projects with descriptions, badges, and icons. Additionally, the Card component's border opacity is adjusted for better visual consistency.

--- app/main/Projects.tsx
+++ app/main/Projects.tsx
@@ -1,4 +1,6 @@
-//import { Card } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { ArrowUpRight } from "lucide-react";
 
 export default function Projects() {
   return (
@@ -11,6 +13,56 @@ export default function Projects() {
           A showcase of real-world projects where design meets code and vision
           becomes experience.
         </h2>
+        <div className="flex flex-wrap mt-12 gap-2.5 md:max-w-3xl lg:max-w-5xl mx-auto">
+          <Card className="flex flex-rows items-center justify-center rounded-2xl bg-card p-5 max-w-sm mx-auto">
+            <div className="w-[350px] h-[289px] rounded-2xl"></div>
+            <div className="ml-2.5">
+              <div>
+                <h3 className="text-xl font-medium">Zenya - Wellness Spa</h3>
+                <Badge className="text-sm bg-white text-black/70 font-normal px-3 py-1.5 mt-2">
+                  Graphic Design
+                </Badge>
+              </div>
+              <div className="flex items-center justify-center gap-4 mt-3">
+                <p className="text-black/70">
+                  A calming and luxurious brand identity for a modern spa
+                  experience.
+                </p>
+                <div className="p-1.5 bg-primary rounded-full mr-2">
+                  <ArrowUpRight
+                    size={32}
+                    strokeWidth={2}
+                    className="text-white"
+                  />
+                </div>
+              </div>
+            </div>
+          </Card>
+          <Card className="flex flex-rows items-center justify-center rounded-2xl bg-card p-5 max-w-sm mx-auto">
+            <div className="w-[350px] h-[289px] rounded-2xl"></div>
+            <div className="ml-2.5">
+              <div>
+                <h3 className="text-xl font-medium">Zenya - Wellness Spa</h3>
+                <Badge className="text-sm bg-white text-black/70 font-normal px-3 py-1.5 mt-2">
+                  Graphic Design
+                </Badge>
+              </div>
+              <div className="flex items-center justify-center gap-4 mt-3">
+                <p className="text-black/70">
+                  A calming and luxurious brand identity for a modern spa
+                  experience.
+                </p>
+                <div className="p-1.5 bg-primary rounded-full mr-2">
+                  <ArrowUpRight
+                    size={32}
+                    strokeWidth={2}
+                    className="text-white"
+                  />
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
       </div>
     </section>
   );
