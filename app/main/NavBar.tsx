468c4af35209c8af85745718edfffcbeecb18022
NgollrRomaric <ngolleromaric27@gmail.com>
2025-05-14 23:17:04 +0100

    style(NavBar): adjust margin-top for better visual alignment
    The margin-top value was reduced from `mt-11` to `mt-8` to improve the visual alignment of the NavBar component with the rest of the layout. This change ensures a more consistent and aesthetically pleasing design.

--- app/main/NavBar.tsx
+++ app/main/NavBar.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 
 export default function NavBar() {
   return (
-    <section className="mt-11 lg:mt-6 sticky top-5 z-50">
+    <section className="mt-8 lg:mt-6 sticky top-5 z-50">
       <div className="flex items-center justify-between p-2.5 border border-black/20 rounded-full max-w-xs md:max-w-xl lg:max-w-5xl mx-auto backdrop-blur bg-white/30">
         <Image
           src={LogoRomaric}
